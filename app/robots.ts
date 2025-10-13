1c3b23f1fedb17cf3426bd3503dca002b01c8a83
Nicolas Piquerez <npiquerez@gmail.com>
2025-10-14 00:27:15 +0200

    rename .fr to .org urls

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://francecrypto.fr/sitemap.xml',
+    sitemap: 'https://francecrypto.org/sitemap.xml',
   }
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
  
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://francecrypto.fr'
+  const baseUrl = 'https://francecrypto.org'
   
   return [
     {
